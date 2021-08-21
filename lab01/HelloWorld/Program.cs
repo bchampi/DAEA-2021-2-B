ee2bf48ef251914cdd37a538fe2885dfb042e79f
Brayan Champi <brayan.champi@tecsup.edu.pe>
2021-08-20 22:42:40 -0500

    Lab01 - Hola Mundo (parte 1)

---
+++ lab01/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, como te llamas?");
+            String name = Console.ReadLine();
+            Console.WriteLine("Mucho gusto en conocerte " + name);
+        }
+    }
+}
