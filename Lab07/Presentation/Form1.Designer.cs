3568fe154eff660badedd7b9d5d9d5dcc0c89686
Brayan Champi <brayan.champi@tecsup.edu.pe>
2021-10-11 12:46:10 -0500

    Actividad adicional buscar campos con estructura de capas(finished)

--- Lab07/Presentation/Form1.Designer.cs
+++ Lab07/Presentation/Form1.Designer.cs
@@ -29,13 +29,67 @@ namespace Presentation
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.textInput = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvData
+            // 
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(12, 52);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.RowHeadersWidth = 51;
+            this.dgvData.RowTemplate.Height = 24;
+            this.dgvData.Size = new System.Drawing.Size(588, 407);
+            this.dgvData.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.Cornsilk;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Font = new System.Drawing.Font("JetBrains Mono", 10F);
+            this.btnSearch.ForeColor = System.Drawing.Color.Coral;
+            this.btnSearch.Location = new System.Drawing.Point(494, 14);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(106, 32);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Buscar";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // textInput
+            // 
+            this.textInput.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textInput.Location = new System.Drawing.Point(304, 15);
+            this.textInput.Name = "textInput";
+            this.textInput.Size = new System.Drawing.Size(183, 30);
+            this.textInput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(612, 471);
+            this.Controls.Add(this.textInput);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.dgvData);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Formulario de Personas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox textInput;
     }
 }
 
--- Lab07/Presentation/Form1.cs
+++ Lab07/Presentation/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Business;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,27 @@ namespace Presentation
 {
     public partial class Form1 : Form
     {
+        DataTable tablePerson = new DataTable();
+        BusinessPerson bp = new BusinessPerson();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tablePerson = bp.GetAll();
+
+            dgvData.DataSource = tablePerson;
+            dgvData.Refresh();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            BusinessPerson bp = new BusinessPerson();
+            tablePerson = bp.SearchPerson(textInput.Text);
+            dgvData.DataSource = tablePerson;
+            dgvData.Refresh();
         }
     }
 }
