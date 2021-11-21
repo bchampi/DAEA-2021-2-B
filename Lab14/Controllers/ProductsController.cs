47fa7766a70f2a0970a674c4029297dd76d4afa1
Brayan Champi <brayan.champi@tecsup.edu.pe>
2021-11-21 16:28:38 -0500

    CRUD terminado de tabla Productos (parte 4)

--- Lab14/Controllers/ProductsController.cs
+++ Lab14/Controllers/ProductsController.cs
@@ -1,37 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Lab14;
 
 namespace Lab14.Controllers
 {
     public class ProductsController : Controller
     {
-        #region Context
-        private NorthwndEntities _context;
+        private NorthwndEntities db = new NorthwndEntities();
 
-        public NorthwndEntities Context
+        // GET: Products1
+        public ActionResult Index()
         {
-            set { _context = value; }
-            get
-            {
-                if (_context == null)
-                    _context = new NorthwndEntities();
-
-                return _context;
-            }
+            var products = db.Products.Include(p => p.Categories).Include(p => p.Suppliers);
+            return View(products.ToList());
         }
-        #endregion
 
-        // GET: Products
-        public ActionResult Details(int? id)
+        public ActionResult ByCategory(int? id)
         {
             if (id == null)
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 
-            var productByCategory = from p in Context.Products
+            var productByCategory = from p in db.Products
                                     orderby p.ProductName ascending
                                     where p.CategoryID == id
                                     select p;
@@ -40,6 +35,119 @@ namespace Lab14.Controllers
                 return HttpNotFound();
 
             return View(productByCategory.ToList());
+        }
+
+        // GET: Products1/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            Products products = db.Products.Find(id);
+            
+            if (products == null)
+                return HttpNotFound();
+
+            return View(products);
+        }
+
+        // GET: Products1/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName");
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName");
+            return View();
+        }
+
+        // POST: Products1/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductID,ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice,UnitsInStock,UnitsOnOrder,ReorderLevel,Discontinued")] Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(products);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", products.CategoryID);
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName", products.SupplierID);
+            
+            return View(products);
+        }
+
+        // GET: Products1/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            Products products = db.Products.Find(id);
+            
+            if (products == null)
+                return HttpNotFound();
+
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", products.CategoryID);
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName", products.SupplierID);
+            
+            return View(products);
+        }
+
+        // POST: Products1/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductID,ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice,UnitsInStock,UnitsOnOrder,ReorderLevel,Discontinued")] Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(products).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", products.CategoryID);
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName", products.SupplierID);
+            
+            return View(products);
+        }
+
+        // GET: Products1/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            Products products = db.Products.Find(id);
+            if (products == null)
+                return HttpNotFound();
+
+            return View(products);
+        }
+
+        // POST: Products1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Products products = db.Products.Find(id);
+            
+            db.Products.Remove(products);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                db.Dispose();
+
+            base.Dispose(disposing);
         }
     }
 }
