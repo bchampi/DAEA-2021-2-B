bf626845b8a82236f57d8da3427fb9e7981f0fd8
Brayan Champi <brayan.champi@tecsup.edu.pe>
2021-11-30 09:48:36 -0500

    CRUD completo de base de datos LIBRARY

---
+++ Practica04/Controllers/PrestamosController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Practica04.Models;
+
+namespace Practica04.Controllers
+{
+    public class PrestamosController : Controller
+    {
+        private libraryEntities1 db = new libraryEntities1();
+
+        // GET: Prestamos
+        public ActionResult Index()
+        {
+            var prestamos = db.Prestamos.Include(p => p.Libro).Include(p => p.Usuario);
+            return View(prestamos.ToList());
+        }
+
+        // GET: Prestamos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prestamos prestamos = db.Prestamos.Find(id);
+            if (prestamos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prestamos);
+        }
+
+        // GET: Prestamos/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdLibro = new SelectList(db.Libro, "IdLibro", "Titulo");
+            ViewBag.IdUsuario = new SelectList(db.Usuario, "IdUsuario", "NIF");
+            return View();
+        }
+
+        // POST: Prestamos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdPrestamo,IdLibro,IdUsuario,FechaPrestamo,FecDevolucion")] Prestamos prestamos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Prestamos.Add(prestamos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdLibro = new SelectList(db.Libro, "IdLibro", "Titulo", prestamos.IdLibro);
+            ViewBag.IdUsuario = new SelectList(db.Usuario, "IdUsuario", "NIF", prestamos.IdUsuario);
+            return View(prestamos);
+        }
+
+        // GET: Prestamos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prestamos prestamos = db.Prestamos.Find(id);
+            if (prestamos == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdLibro = new SelectList(db.Libro, "IdLibro", "Titulo", prestamos.IdLibro);
+            ViewBag.IdUsuario = new SelectList(db.Usuario, "IdUsuario", "NIF", prestamos.IdUsuario);
+            return View(prestamos);
+        }
+
+        // POST: Prestamos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdPrestamo,IdLibro,IdUsuario,FechaPrestamo,FecDevolucion")] Prestamos prestamos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(prestamos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdLibro = new SelectList(db.Libro, "IdLibro", "Titulo", prestamos.IdLibro);
+            ViewBag.IdUsuario = new SelectList(db.Usuario, "IdUsuario", "NIF", prestamos.IdUsuario);
+            return View(prestamos);
+        }
+
+        // GET: Prestamos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prestamos prestamos = db.Prestamos.Find(id);
+            if (prestamos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prestamos);
+        }
+
+        // POST: Prestamos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Prestamos prestamos = db.Prestamos.Find(id);
+            db.Prestamos.Remove(prestamos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
