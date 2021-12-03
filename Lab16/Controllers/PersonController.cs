86dc7c91fb36309912fd90f3781a4ca87c6583c0
Brayan Champi <brayan.champi@tecsup.edu.pe>
2021-12-03 14:20:06 -0500

    Instalación de EntityFrameworkCore para conexión a SqlServer

---
+++ Lab16/Controllers/PersonController.cs
@@ -0,0 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using persons.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
